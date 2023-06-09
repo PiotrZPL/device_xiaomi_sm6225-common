ec2f258f7ea2c6bc8e9475a06946df4927b2a792
Agatha q <naufalmarjan2014@gmail.com>
2023-06-09 16:49:12 +0000

    sm6225-common:: usb: Switch to prebuilt USB configs

--- bengal.mk
+++ bengal.mk
@@ -493,8 +493,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.2-service-xiaomi_bengal \
-    android.hardware.usb.gadget@1.0-service-xiaomi_bengal
+    android.hardware.usb@1.2.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
